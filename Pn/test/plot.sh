c73128993cad3909e03f13fe2607fa144db4fe94
Zhuoran Han <zhuoranh@mit.edu>
2018-07-11 16:48:30 -0500

    rm

--- Pn/test/plot.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python ploterror.py 100 1000 10000 100000 1000000
--- Pn/test/runcalcerr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python calc_err.py 1000000
-python calc_err.py 100
-python calc_err.py 1000
-python calc_err.py 10000
-python calc_err.py 100000
